12d5ec2a5fafc88b85f0b470dc813ab97c68c65b
MD AL AMIN <mdalaminbey@gmail.com>
2022-12-24 20:36:37 +0600

    fix drawer animation issue

--- src/Components/Drawer.php
+++ src/Components/Drawer.php
@@ -19,9 +19,31 @@ class Drawer extends ComponentBase
 	public function content()
 	{
 	?>
-		<div class="drawer" x-cloak x-show="$store.DoatKolomUiDrawer.status" x-trap.noscroll.inert="$store.DoatKolomUiDrawer.status">
-			<div class="fixed top-0 left-0 w-full h-screen z-[9999] bg-black bg-opacity-50">
-				<div x-show="$store.DoatKolomUiDrawer.status" x-transition:enter="transition ease-out duration-500" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in duration-500" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" x-on:click.outside="$store.DoatKolomUiDrawer.changeStatus()" class="fixed top-0 right-0 h-screen bg-white overflow-scroll" :style="$store.DoatKolomUiDrawer.getWidth()">
+		<div 
+			class="drawer z-[9999]" 
+			x-cloak
+			x-transition:enter="transition ease-out duration-300"
+			x-transition:enter-start="opacity-0"
+			x-transition:enter-end="opacity-100"
+			x-transition:leave="transition ease-in duration-300"
+			x-transition:leave-start="opacity-100"
+			x-transition:leave-end="opacity-0"
+			x-show="$store.DoatKolomUiDrawer.status" 
+			x-trap.noscroll.inert="$store.DoatKolomUiDrawer.status"
+		>
+			<div class="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50">
+				<div 
+					x-show="$store.DoatKolomUiDrawer.status" 
+					x-transition:enter="transition ease-out duration-300" 
+					x-transition:enter-start="translate-x-full" 
+					x-transition:enter-end="translate-x-0" 
+					x-transition:leave="transition ease-in duration-300" 
+					x-transition:leave-start="translate-x-0" 
+					x-transition:leave-end="translate-x-full" 
+					x-on:click.outside="$store.DoatKolomUiDrawer.changeStatus()" 
+					class="fixed top-0 right-0 h-screen bg-white overflow-scroll" 
+					:style="$store.DoatKolomUiDrawer.getWidth()"
+				>
 					<div class="doatkolom-ui-drawer-content">
 						<div class="mt-8">
 							<button x-on:click="$store.DoatKolomUiDrawer.changeStatus()" class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-500 text-white pointer-events-auto">X</button>
